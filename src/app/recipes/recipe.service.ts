08908f04e5bd4e85762f127ae0315e3f86dcb6e6
Mahmoud Elshazly <Mahmoud.Elshazly@brmaja.com>
2021-10-06 18:40:01 +0200

    adding services to project

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,49 @@
+import {Recipe} from "./recipe.model";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingrdiant} from "../shared/Ingrediant.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable()
 export class RecipeService
 {
 
+  constructor(private slService:ShoppingListService) {
+  }
+
+  private Recipes : Recipe[] = [
+    new Recipe('First Recipe',
+      'This is simply test for recipe',
+      'https://www.shaadidukaan.com/vogue/wp-content/uploads/2019/08/hug-kiss-images.jpg',
+      [
+      new Ingrdiant('ingrdiant one',5),
+      new Ingrdiant('ingrdiant two',5),
+      new Ingrdiant('ingrdiant three',5),
+    ]),
+    new Recipe('Second Recipe',
+      'This is simply test for recipe to move between them',
+      'https://img.freepik.com/free-photo/cool-geometric-triangular-figure-neon-laser-light-great-backgrounds-wallpapers_181624-9331.jpg?size=626&ext=jpg',
+      [
+        new Ingrdiant('ingrdiant one',9),
+        new Ingrdiant('ingrdiant two',8),
+        new Ingrdiant('ingrdiant three',4),
+      ]),
+    new Recipe('Third Recipe',
+      'This is simply test for recipe to move to the third recipe',
+      'https://www.filmibeat.com/ph-big/2019/07/ismart-shankar_156195627930.jpg',
+        [
+        new Ingrdiant('ingrdiant one',7),
+          new Ingrdiant('ingrdiant two',8),
+          new Ingrdiant('ingrdiant three',9),
+        ])
+  ]
+
+  selectedRecipe = new EventEmitter<Recipe>();
+
+  getRecipes(){
+    return this.Recipes;
+  }
+
+  addIngerdiantsToShoppingList(ingridants:Ingrdiant[]){
+    this.slService.addListOfIngerdiants(ingridants);
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,36 @@
+import {Ingrdiant} from "../shared/Ingrediant.model";
+import {EventEmitter} from "@angular/core";
+
 export class ShoppingListService{
+  ingerdiantsChanged = new EventEmitter<Ingrdiant[]>();
+
+  private ingrediants : Ingrdiant[] = [
+    new Ingrdiant('First Item',5),
+    new Ingrdiant('Secounf Item',10),
+  ]
+
+  getIngeridiants () {
+    /**
+     * this line will pass the data by referance
+     * */
+    /* return this.ingrediants;  */
+
+    /**
+     * this line will be call by value it will send a copy
+     * */
+    return this.ingrediants.slice();
+  }
+  /*
+  because of using slice we have to let components know that we add new ingerdiatnt and i will do that using event Emitter
+  * */
+  addNewIngeridiant (ingerdiant : Ingrdiant){
+    this.ingrediants.push(ingerdiant);
+    this.ingerdiantsChanged.emit(this.ingrediants.slice());
+  }
+
+  addListOfIngerdiants(ingridants:Ingrdiant[]){
+    this.ingrediants.push(...ingridants);
+    this.ingerdiantsChanged.emit(this.ingrediants.slice())
+  }
 
 }
